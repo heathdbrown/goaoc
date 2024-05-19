a893c2b8d13e70e962531e58333f30fe2a470f98
Heath Brown <heathd.brown@gmail.com>
2024-05-19 10:10:53 -0500

    day1 another solution

--- 2015/day1/main.go
+++ 2015/day1/main.go
@@ -15,6 +15,37 @@ func floorChange(s string) int {
 	}
 	return f
 }
+
+func floorChange2(s string) int {
+	seq := "_"
+	c := '_'
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == '{' || s[i] == '[' || s[i] == '(' {
+			if s[i] == '{' {
+				c = '}'
+			} else if s[i] == '[' {
+				c = ']'
+			} else {
+				c = ')'
+			}
+			seq = string(c) + seq
+		} else {
+			if seq[0] != s[i] {
+				return false
+			} else {
+				seq = seq[1:]
+			}
+		}
+	}
+
+	if seq == "_" {
+		return true
+	}
+
+	return false
+}
+
 // ( - up
 // ) - down
 func main() {
